c5d907e0973b15fd38d2cf3b2c9556ebddec7715
jchu4483 <jchu4483@gmail.com>
2016-12-15 23:59:28 -0500

    Done.

---
+++ js/app/controllers/MainController.js
@@ -0,0 +1,7 @@
+function MainController($scope) {
+  $scope.name = "Jason"
+  $scope.email = "jchu4483@gmail.com"
+  $scope.phone = "347-400-1861"
+}
+
+app.controller('MainController', MainController)
